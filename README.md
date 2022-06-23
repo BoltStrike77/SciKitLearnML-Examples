1bc62a8242b67df48b54e8f5fc9af46be9aa041c
Advay Vyas <advay.vyas@gmail.com>
2022-06-22 19:04:58 -0500

    Pipeline complete + housekeeping

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SciKitLearnML-WineExample
+
+A supervised regression machine learning model to tune a random forest for wine quality based on traits. Procedure from https://elitedatascience.com/python-machine-learning-tutorial-scikit-learn. Data from https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/.
--- WineML.py
+++ WineML.py
@@ -8,7 +8,6 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error, r2_score
 import joblib
 
-
 data = pd.read_csv(r"C:\Users\advay\Downloads\winequality-white.csv", sep=';')
 
 print("\nDescription of data in csv:")
@@ -21,20 +20,36 @@ X = data.drop('quality', axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123, stratify=y)
 
 # preprocess and scale data
-scaler = preprocessing.StandardScaler().fit(X_train)
+scaler = preprocessing.StandardScaler().fit(X_train) #create scaler
 X_train_scaled = scaler.transform(X_train)
-
 print("\nMean of scaled training data:")
 print (X_train_scaled.mean(axis=0))
-
 print("\nStandard Dev of scaled training data:")
 print (X_train_scaled.std(axis=0))
 
 # transform test set using training set scale
 X_test_scaled = scaler.transform(X_test)
-
 print("\nMean of scaled test data:")
 print (X_test_scaled.mean(axis=0))
-
 print("\nStandard Dev of scaled test data:")
 print (X_test_scaled.std(axis=0))
+
+# create pipeline
+pipeline = make_pipeline(preprocessing.StandardScaler(), RandomForestRegressor(n_estimators=100))
+
+# setting up hyperparameters
+print("\nParameters:")
+print (pipeline.get_params())
+hyperparameters = { 'randomforestregressor__max_features' : ['auto', 'sqrt', 'log2'],
+                  'randomforestregressor__max_depth': [None, 5, 3, 1]}
+
+# cross validation pipeline with SciKit
+
+clf = GridSearchCV(pipeline, hyperparameters, cv=10)
+clf.fit(X_train, y_train)  # fit and tune model            
+
+print("\nBest Parameters:")
+print(clf.best_params_) # best params
+
+print("\nRefit pipline with better hyperparameters")
+print(clf.refit) # refit on best hyperparameters
